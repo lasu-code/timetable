7a42583db621afe92faba68c2bbc278871eaed2e
Olabiran Joshua <olabiranj@gmail.com>
2018-10-20 15:32:54 +0100

    corrected constName

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const timeController = require('../controllers/timeTableController');
+const timeTableController = require('../controllers/timeTableController');
 
 /* GET home page. */
 // router.get('/', function(req, res, next) {
