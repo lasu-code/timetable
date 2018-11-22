ea22d185df53596da6ecd652177cb5a980a6576a
AbdRozaq Iwashokun <ade.iwashokun@triatronltd.com>
2018-11-22 04:06:40 +0100

    flash message in passport config, code cleanup

--- config/passport.js
+++ config/passport.js
@@ -13,58 +13,61 @@ passport.deserializeUser(function (id, done) {
     })
 })
 
-passport.use('local.registerUser', new LocalStrategy({
-    usernameField: "schEmail",
-    passwordField: "password",
-    passReqToCallback: true
-}, function (req, schEmail, password, done) {
-    User.findOne({ 'schEmail': schEmail }, function (err, user) {
-        if (err) {
-            return done(err);
-        }
-        if (user) {
-            return done(null, false)
-        }
-
-        let newUser = new User();
-        newUser.schEmail = req.body.schEmail;
-        newUser.address = req.body.address;
-        newUser.schName = req.body.schName;
-        newUser.adminName = req.body.adminName
-        newUser.password = newUser.generateHash(req.body.password);
-        newUser.role = req.body.role
-
-        newUser.save(function (err) {
+passport.use('local.registerUser',
+    new LocalStrategy({
+        usernameField: "schEmail",
+        passwordField: "password",
+        passReqToCallback: true
+    },
+    function (req, schEmail, password, done) {
+        User.findOne({ 'schEmail': schEmail }, function (err, user) {
             if (err) {
-                return done(err)
+                return done(err);
+            }
+            if (user) {
+                req.flash('error', 'User account exist, login instead');
+                return done(null, false)
             }
 
-            return done(null, newUser)
+            let newUser = new User();
+            newUser.schEmail = req.body.schEmail;
+            newUser.address = req.body.address;
+            newUser.schName = req.body.schName;
+            newUser.adminName = req.body.adminName
+            newUser.password = newUser.generateHash(req.body.password);
+
+            newUser.save(function (err) {
+                if (err) {
+                    return done(err)
+                }
+
+                return done(null, newUser)
+            })
         })
     })
-}))
+);
 
 
-passport.use('local.loginUser', new LocalStrategy({
-    usernameField: "schEmail",
-    passwordField: "password",
-    passReqToCallback: true
-}, function (req, schEmail, password, done) {
-    User.findOne({ 'schEmail': schEmail}, function (err, user) {
-        if (err) {
-            return done(err);
-        }
-        if (!user) {
-           
-            return done(null, false)
-        }
-
-        if (!user.validatePassword(req.body.password)) {
-            
-            return done(null, false)
-        }
-
-        return done(null, user)
-
+passport.use('local.loginUser',
+    new LocalStrategy({
+        usernameField: "schEmail",
+        passwordField: "password",
+        passReqToCallback: true
+    },
+    function (req, schEmail, password, done) {
+        User.findOne({ 'schEmail': schEmail}, function (err, user) {
+            if (err) {
+                return done(err);
+            }
+            if (!user) {
+                req.flash('error', "User record not found!");
+                return done(null, false)
+            }
+            if (!user.validatePassword(password)) {
+                req.flash('error', 'Invalid user password!');
+                return done(null, false)
+            }
+            return done(null, user)
+        })
     })
-}))
+)
